baf12f3d8025bd9188d5cf12826a423f79d56221
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2020-01-02 08:52:49 -0300

    Added first activity in flow

--- SkeletonMVVM/buildSrc/src/main/java/dependencies/Modules.kt
+++ SkeletonMVVM/buildSrc/src/main/java/dependencies/Modules.kt
@@ -4,4 +4,6 @@ package dependencies
  * Created by bruno on 2020-01-02
  */
 object Modules {
+    const val STYLEGUIDE = ":styleGuide"
+    const val PHOTOLIST = ":photoList"
 }
---
+++ SkeletonMVVM/photoList/src/main/java/io/github/brunogabriel/photolist/PhotoListActivity.kt
@@ -0,0 +1,14 @@
+package io.github.brunogabriel.photolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by bruno on 2020-01-02
+ */
+class PhotoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_list)
+    }
+}
